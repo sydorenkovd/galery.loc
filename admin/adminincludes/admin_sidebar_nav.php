b4d88d67e288d00e547b00f2eca18939c8e60392
sydorenkovd <sydorenkovd@gmail.com>
2015-12-22 21:39:36 +0200

    base application

---
+++ admin/adminincludes/admin_sidebar_nav.php
@@ -0,0 +1,23 @@
+<!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li class="active">
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="user.php"><i class="fa fa-fw fa-bar-chart-o"></i> Users</a>
+        </li>
+        <li>
+            <a href="upload.php"><i class="fa fa-fw fa-table"></i> Upload</a>
+        </li>
+        <li>
+            <a href="comment.php"><i class="fa fa-fw fa-edit"></i> Comments</a>
+        </li>
+        <li>
+            <a href="photos.php"><i class="fa fa-fw fa-edit"></i> Photos</a>
+        </li>
+
+    </ul>
+</div>
+<!-- /.navbar-collapse -->
+</nav>
---
+++ admin/user.php
@@ -0,0 +1,43 @@
+<?php include_once "adminincludes/admin_header.php"?>
+
+    <div id="wrapper">
+
+    <!-- Navigation -->
+<?php include_once "adminincludes/admin_top_nav.php"?>
+
+<?php include_once "adminincludes/admin_sidebar_nav.php"?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Users <small>Statistics Overview</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li class="active">
+                            <i class="fa fa-dashboard"></i> Dashboard
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <!-- /.row -->
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="alert alert-info alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <i class="fa fa-info-circle"></i>  <strong>Like SB Admin?</strong> Try out <a href="http://startbootstrap.com/template-overviews/sb-admin-2" class="alert-link">SB Admin 2</a> for additional features!
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+
+
+        </div>
+        <!-- /.container-fluid -->
+
+<?php include_once "adminincludes/admin_footer.php"?>
